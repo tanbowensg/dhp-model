95d9bbfc3646c25e461708c7ee93005302935dc1
tanbowen <tanbowensg@gmail.com>
2017-04-18 10:41:30 +0800

    把 app 格式化的过程改成 class

--- stream/app.stream.js
+++ stream/app.stream.js
@@ -1,23 +1,18 @@
 // 这个 app 的流负责所有的 app 的格式化。
 // 输入应该是后端发回来的 raw 的数据
 // 返回的是可以直接供前端渲染的数据
-
 const Rx = require('rxjs/Rx');
 const appApi = require('../api/app.js');
+const AppClass = require('../factory/app.js').App;
 
 const app$ = new Rx.BehaviorSubject();
-
-function formatApp(app) {
-  const result = {
-    name: app.Name,
-  };
-  return result;
-}
 
 function getApps() {
   return appApi.appList()
     .then(res => {
-      return res.map(formatApp);
+      return res.map(app => {
+        return new AppClass(app);
+      });
     })
     .then(res => {
       app$.next(res);
