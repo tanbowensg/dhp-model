0a04e21f5f7aca6a30a8bad5105f776f36d58d54
tanbowen <tanbowensg@gmail.com>
2017-04-24 17:34:45 +0800

    新增 task 的 model

---
+++ factory/task.js
@@ -0,0 +1,13 @@
+const _ = require('lodash');
+const moment = require('moment');
+
+class Task {
+  constructor(task) {
+    this._init(task);
+  }
+
+  _init(task) {
+  }
+}
+
+exports.Task = Task;
