722be4a86b9ddf69eaa6535db81d378120ddc14e
tanbowen <tanbowensg@gmail.com>
2017-04-27 11:23:03 +0800

    新增 alpha$ 和 socket$

--- stream/hub.js
+++ stream/hub.js
@@ -1,40 +1,43 @@
 // 这是一个集线器，也是一个数据池
-const appStream = require('./app.stream.js');
-const serviceStream = require('./service.stream.js');
-const taskStream = require('./task.stream.js');
-const infoStream = require('./info.stream.js');
+const Rx = require('rxjs/Rx');
 
+// const appStream = require('./app.stream.js');
+// const serviceStream = require('./service.stream.js');
+// const taskStream = require('./task.stream.js');
+const infoStream = require('./info.stream.js');
 const socketio = require('../util/socket.js');
 
-const hub = {
-  apps$: appStream.app$,
-  services$: serviceStream.services$,
-  tasks$: taskStream.tasks$,
-  apiInfo$: infoStream.apiInfo$,
-};
+const JobClass = require('../factory/job.js').Job;
 
+const appApi = require('../api/app.js');
 
-// 整个应用初始化的地方，有些 api 是要先于其他 api 调用的
-function init(callback) {
-  infoStream.getApiInfo();
-  const socketio$$ = hub.apiInfo$.subscribe(info => {
-    callback(info);
-    socketio$$.unsubscribe();
+// 此乃一切 Observable 和 Subject 起点，故名 alpha ———— 博文
+const alpha$ = new Rx.BehaviorSubject().filter(v => v);
+// socket 紧跟在 alpha 之后
+const socket$ = new Rx.Subject().map(job => new JobClass(job));
+
+// 但是有些 API 先于 alpha 存在（比如 apiInfo），因此它们叫 zero。
+// 但是它们不变不灭，也不需要被感知。 ———— 博文
+(function zero() {
+  Rx.Observable.combineLatest(infoStream.getApiInfo())
+    // 下面是个数组，由于现在只有一个元素，所以就简单点来吧
+    .map(array => {
+      return array[0]
+    })
+    .subscribe(alpha$);
+})();
+
+alpha$.subscribe(apiInfo => {
+  socketio.connect(apiInfo.StreamRoom)
+  socketio.bind('observable', job => {
+    socket$.next(job);
   });
-}
-
-init(info => {
-  socketio.connect(info.StreamRoom);
-  socketio.bind('test', job => {
-    console.log(job);
-  });
-  serviceStream.getServices();
-  appStream.getApps();
-  taskStream.getTasks();
 });
 
-// const reducers = {
-//   appUpdate:
-// }
+const hub = {
+  apps$: socket$.filter(job => job.Entity.ObjectType === 'Application'),
+  services$: socket$.filter(job => job.Entity.ObjectType === 'Service'),
+  tasks$: socket$.filter(job => job.Entity.ObjectType === 'Task'),
+};
 
 exports.hub = hub;
--- stream/info.stream.js
+++ stream/info.stream.js
@@ -4,12 +4,13 @@ const infoApi = require('../api/info.js');
 const apiInfo$ = new Rx.BehaviorSubject().filter(v => v);
 
 function getApiInfo() {
-  Rx.Observable.fromPromise(infoApi.apiInfo())
-    .subscribe(apiInfo => {
-      apiInfo$.next(apiInfo);
-    }, rej => {
-      console.log(rej);
-    });
+  const observable = Rx.Observable.fromPromise(infoApi.apiInfo());
+  observable.subscribe(apiInfo => {
+    apiInfo$.next(apiInfo);
+  }, rej => {
+    console.log(rej);
+  });
+  return observable;
 }
 
 exports.getApiInfo = getApiInfo;
